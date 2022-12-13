b9724a50694dc492000adf954d53c15c9931b55d
luizaalcm <114439288+luizaalcm@users.noreply.github.com>
2022-12-13 10:30:53 -0300

    aprendendo

---
+++ ex1/script.js
@@ -0,0 +1,8 @@
+
+
+document.getElementById("titulo").innerHTML = "Opa,tudo bem?";
+document.write("Algum texto");
+
+window.alert("mensagem de exemplo");
+
+console.log("a mensagem que eu inserir aqui aparecerá no console");

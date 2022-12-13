d1d6efbdc8da28d30e003352ef6bf63bbf3c2250
luizaalcm <114439288+luizaalcm@users.noreply.github.com>
2022-12-13 11:00:13 -0300

    estudos

---
+++ ex2/script.js
@@ -0,0 +1,19 @@
+var idade = 90;
+
+// a variavél idade tem o valor 90*/
+
+var nome = "Bonieky"; //STRING - texto, entre aspas. Os textos tem que estar sempre entre "", não se pode realizar somas com numeros entre aspas também.*/
+
+var sobrenome = "Lacerda";
+
+var nomecompleto = nome +" "+ sobrenome; //dando espaço*/
+
+
+alert(nomecompleto);
+
+var x = 10;
+var y = 15;
+
+var total = x + y;
+
+

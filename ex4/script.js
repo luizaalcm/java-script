a31ab8097600525e262f3693b6eddcaf65fcd150
luizaalcm <114439288+luizaalcm@users.noreply.github.com>
2022-12-13 12:30:29 -0300

    estudos

---
+++ ex4/script.js
@@ -0,0 +1,10 @@
+var nome = "Bonieky";
+// HOISTING variavel global 
+
+
+let nome = "Bonieky";
+// variavel disponivel apenas naquela parte do codigo, no escopo especifico
+
+const nome = "Bonieky";
+
+// não podemos dar um novo valor pra uma variavel  constante 
